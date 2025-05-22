2a4299562752b3fd08c77331e4f4c237c9812bec
Ryon McAuley <rdmcauley@gmail.com>
2025-05-22 07:23:52 -0400

    hack font in wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,7 +10,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- config.font = wezterm.font("JetBrains Mono")
-config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
+-- config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
+config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 18.0
 config.window_background_opacity = 0.9
 
